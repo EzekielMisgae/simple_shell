41bd85eddefa33d46bc333a5b7192fd015d1603b
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-06-15 20:54:19 +0300

    com1

--- simple_shell.h
+++ simple_shell.h
@@ -1,80 +1,52 @@
 #ifndef _SIMPLE_SHELL_H_
 #define _SIMPLE_SHELL_H_
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
-#include <limits.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdbool.h>
+
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+extern char **environ;
 
 /**
- * struct variables - variables
- * @av: command line arguments
- * @buffer: buffer of command
- * @env: environment variables
- * @count: count of commands entered
- * @argv: arguments at opening of shell
- * @status: exit status
- * @commands: commands to execute
+ * struct builtins - Handles builtins
+ * @env: First member
+ * @exit: Second member
+ *
+ * Description: builtin commands
  */
-typedef struct variables
+
+struct builtins
 {
-	char **av;
-	char *buffer;
-	char **env;
-	size_t count;
-	char **argv;
-	int status;
-	char **commands;
-} vars_t;
+	char *env;
+	char *exit;
 
-/**
- * struct builtins - struct for the builtin functions
- * @name: name of builtin command
- * @f: function for corresponding builtin
- */
-typedef struct builtins
-{
-	char *name;
-	void (*f)(vars_t *);
-} builtins_t;
+} builtins;
 
-char **make_env(char **env);
-void free_env(char **env);
+int _strlen(const char *str);
+char *_strchr(char *str, int c);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *first, const char *second, int n);
+char *_strdup(char *str);
+void free_args(char **m);
+char **token_maker(char *str);
+void exec_cmd(char *command, char **args);
+void exiter(char **arr, char *line);
+void print_env(void);
+int is_builtin(char **command, char *line);
+char *pathcat(char *path, char *command);
+char *ver_paths(char **p, char *command);
+char *_getpath(void);
+int check_type(char **args, char *line);
+void init_shell(void);
+int main(int argc, char **argv, char **envp);
 
-ssize_t _puts(char *str);
-char *_strdup(char *strtodup);
-int _strcmpr(char *strcmp1, char *strcmp2);
-char *_strcat(char *strc1, char *strc2);
-unsigned int _strlen(char *str);
-
-char **tokenize(char *buffer, char *delimiter);
-char **_realloc(char **ptr, size_t *size);
-char *new_strtok(char *str, const char *delim);
-
-void (*check_for_builtins(vars_t *vars))(vars_t *vars);
-void new_exit(vars_t *vars);
-void _env(vars_t *vars);
-void new_setenv(vars_t *vars);
-void new_unsetenv(vars_t *vars);
-
-void add_key(vars_t *vars);
-char **find_key(char **env, char *key);
-char *add_value(char *key, char *value);
-int _atoi(char *str);
-
-void check_for_path(vars_t *vars);
-int path_execute(char *command, vars_t *vars);
-char *find_path(char **env);
-int execute_cwd(vars_t *vars);
-int check_for_dir(char *str);
-
-void print_error(vars_t *vars, char *msg);
-void _puts2(char *str);
-char *_uitoa(unsigned int count);
-
-#endif /* _SIMPLE_SHELL_H_ */
+#endif /* _SIMPLE_SHELL_H */
