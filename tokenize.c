dd444b00f7a526e3ca81c364f68e71931a6c3e95
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-06-15 13:50:37 +0300

    commit

--- tokenize.c
+++ tokenize.c
@@ -1,12 +1,12 @@
-#include "shell.h"
+#include "simple_shell.h"
 
 /**
-* tokenize - tokenizes a buffer with a delimiter
-* @buffer: buffer to tokenize
-* @delimiter: delimiter to tokenize along
-* Return: pointer to an array of pointers to the tokens
-*/
-
+ * tokenize - tokenizes a buffer with a delimiter
+ * @buffer: buffer to tokenize
+ * @delimiter: delimiter to tokenize along
+ *
+ * Return: pointer to an array of pointers to the tokens
+ */
 char **tokenize(char *buffer, char *delimiter)
 {
 	char **tokens = NULL;
